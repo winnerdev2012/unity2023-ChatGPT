winnerdev2012/unity2023-ChatGPT

commit 64c82c9b25f28e7ba909f5f88a108ef7d351807c

Introduce 'AddressOf' pseudo-opcode to make the type system aware of inlined value types (#139)

ICSharpCode.Decompiler/Ast/AstMethodBodyBuilder.cs
@@ -743,6 +743,8 @@ namespace ICSharpCode.Decompiler.Ast
 					}
 				case ILCode.InitializedObject:
 					return new InitializedObjectExpression();
+				case ILCode.AddressOf:
+					return MakeRef(arg1);
 				default:
 					throw new Exception("Unknown OpCode: " + byteCode.Code);
 			}

ICSharpCode.Decompiler/ILAst/ILAstOptimizer.cs
@@ -291,32 +291,52 @@ namespace ICSharpCode.Decompiler.ILAst
 		/// 
 		/// CallGetter/CallSetter is used to allow the ILAst to represent "while ((SomeProperty = value) != null)".
 		/// </summary>
-		void IntroducePropertyAccessInstructions(ILBlock method)
+		void IntroducePropertyAccessInstructions(ILNode node)
 		{
-			foreach (ILExpression expr in method.GetSelfAndChildrenRecursive<ILExpression>()) {
-				if (expr.Code == ILCode.Call || expr.Code == ILCode.Callvirt) {
-					MethodReference cecilMethod = (MethodReference)expr.Operand;
-					if (cecilMethod.DeclaringType is ArrayType) {
-						switch (cecilMethod.Name) {
-							case "Get":
-								expr.Code = ILCode.CallGetter;
-								break;
-							case "Set":
-								expr.Code = ILCode.CallSetter;
-								break;
-							case "Address":
-								expr.Code = ILCode.CallGetter;
-								expr.AddPrefix(new ILExpressionPrefix(ILCode.PropertyAddress));
-								break;
-						}
-					} else {
-						MethodDefinition cecilMethodDef = cecilMethod.Resolve();
-						if (cecilMethodDef != null) {
-							if (cecilMethodDef.IsGetter)
-								expr.Code = (expr.Code == ILCode.Call) ? ILCode.CallGetter : ILCode.CallvirtGetter;
-							else if (cecilMethodDef.IsSetter)
-								expr.Code = (expr.Code == ILCode.Call) ? ILCode.CallSetter : ILCode.CallvirtSetter;
-						}
+			ILExpression parentExpr = node as ILExpression;
+			if (parentExpr != null) {
+				for (int i = 0; i < parentExpr.Arguments.Count; i++) {
+					ILExpression expr = parentExpr.Arguments[i];
+					IntroducePropertyAccessInstructions(expr);
+					IntroducePropertyAccessInstructions(expr, parentExpr, i);
+				}
+			} else {
+				foreach (ILNode child in node.GetChildren()) {
+					IntroducePropertyAccessInstructions(child);
+					ILExpression expr = child as ILExpression;
+					if (expr != null) {
+						IntroducePropertyAccessInstructions(expr, null, -1);
+					}
+				}
+			}
+		}
+		
+		void IntroducePropertyAccessInstructions(ILExpression expr, ILExpression parentExpr, int posInParent)
+		{
+			if (expr.Code == ILCode.Call || expr.Code == ILCode.Callvirt) {
+				MethodReference cecilMethod = (MethodReference)expr.Operand;
+				if (cecilMethod.DeclaringType is ArrayType) {
+					switch (cecilMethod.Name) {
+						case "Get":
+							expr.Code = ILCode.CallGetter;
+							break;
+						case "Set":
+							expr.Code = ILCode.CallSetter;
+							break;
+						case "Address":
+							expr.Code = ILCode.CallGetter;
+							if (parentExpr != null) {
+								parentExpr.Arguments[posInParent] = new ILExpression(ILCode.AddressOf, null, expr);
+							}
+							break;
+					}
+				} else {
+					MethodDefinition cecilMethodDef = cecilMethod.Resolve();
+					if (cecilMethodDef != null) {
+						if (cecilMethodDef.IsGetter)
+							expr.Code = (expr.Code == ILCode.Call) ? ILCode.CallGetter : ILCode.CallvirtGetter;
+						else if (cecilMethodDef.IsSetter)
+							expr.Code = (expr.Code == ILCode.Call) ? ILCode.CallSetter : ILCode.CallvirtSetter;
 					}
 				}
 			}

ICSharpCode.Decompiler/ILAst/ILInlining.cs
@@ -73,7 +73,7 @@ namespace ICSharpCode.Decompiler.ILAst
 			}
 			foreach(ILBasicBlock bb in body.OfType<ILBasicBlock>()) {
 				modified |= InlineAllInBasicBlock(bb);
-		}
+			}
 			return modified;
 		}
 		
@@ -183,31 +183,74 @@ namespace ICSharpCode.Decompiler.ILAst
 			ILExpression parent;
 			int pos;
 			if (FindLoadInNext(next as ILExpression, v, inlinedExpression, out parent, out pos) == true) {
-				if (ldloc == 0)
-				{
+				if (ldloc == 0) {
 					if (!IsGeneratedValueTypeTemporary((ILExpression)next, parent, pos, v))
 						return false;
+				} else {
+					if (!aggressive && !v.IsGenerated && !NonAggressiveInlineInto((ILExpression)next, parent, inlinedExpression))
+						return false;
 				}
-				else if (!aggressive && !v.IsGenerated && !NonAggressiveInlineInto((ILExpression)next, parent, inlinedExpression))
-					return false;
 				
 				// Assign the ranges of the ldloc instruction:
 				inlinedExpression.ILRanges.AddRange(parent.Arguments[pos].ILRanges);
 				
-				parent.Arguments[pos] = inlinedExpression;
-				
+				if (ldloc == 0) {
+					// it was an ldloca instruction, so we need to use the pseudo-opcode 'addressof' so that the types
+					// comes out correctly
+					parent.Arguments[pos] = new ILExpression(ILCode.AddressOf, null, inlinedExpression);
+				} else {
+					parent.Arguments[pos] = inlinedExpression;
+				}
 				return true;
 			}
 			return false;
 		}
 
 		/// <summary>
-		/// Is this a temporary variable generated by the C# compiler for instance method calls on immutable value type values
+		/// Is this a temporary variable generated by the C# compiler for instance method calls on value type values
 		/// </summary>
+		/// <param name="next">The next top-level expression</param>
+		/// <param name="parent">The direct parent of the load within 'next'</param>
+		/// <param name="pos">Index of the load within 'parent'</param>
+		/// <param name="v">The variable being inlined.</param>
 		bool IsGeneratedValueTypeTemporary(ILExpression next, ILExpression parent, int pos, ILVariable v)
 		{
-			return pos == 0 && v.Type != null && v.Type.IsValueType && next.Code == ILCode.Stloc
-				&& (parent.Code == ILCode.Call || parent.Code == ILCode.Callvirt) && ((MethodReference)parent.Operand).HasThis;
+			if (pos == 0 && v.Type != null && v.Type.IsValueType) {
+				// inline the compiler-generated variable that are used when accessing a member on a value type:
+				switch (parent.Code) {
+					case ILCode.Call:
+					case ILCode.CallGetter:
+					case ILCode.CallSetter:
+					case ILCode.Callvirt:
+					case ILCode.CallvirtGetter:
+					case ILCode.CallvirtSetter:
+						MethodReference mr = (MethodReference)parent.Operand;
+						return mr.HasThis;
+					case ILCode.Stfld:
+					case ILCode.Ldfld:
+					case ILCode.Ldflda:
+						return true;
+				}
+			}
+			return false;
+		}
+		
+		/// <summary>
+		/// Determines whether a variable should be inlined in non-aggressive mode, even though it is not a generated variable.
+		/// </summary>
+		/// <param name="next">The next top-level expression</param>
+		/// <param name="parent">The direct parent of the load within 'next'</param>
+		bool NonAggressiveInlineInto(ILExpression next, ILExpression parent)
+		{
+			switch (next.Code) {
+				case ILCode.Ret:
+				case ILCode.Brtrue:
+					return parent == next;
+				case ILCode.Switch:
+					return parent == next || (parent.Code == ILCode.Sub && parent == next.Arguments[0]);
+				default:
+					return false;
+			}
 		}
 		
 		bool NonAggressiveInlineInto(ILExpression next, ILExpression parent, ILExpression inlinedExpression)

ICSharpCode.Decompiler/Ast/Transforms/ReplaceMethodCallsWithOperators.cs
@@ -256,7 +256,7 @@ namespace ICSharpCode.Decompiler.Ast.Transforms
 		
 		static bool IsWithoutSideEffects(Expression left)
 		{
-			return left is ThisReferenceExpression || left is IdentifierExpression || left is TypeReferenceExpression;
+			return left is ThisReferenceExpression || left is IdentifierExpression || left is TypeReferenceExpression || left is BaseReferenceExpression;
 		}
 		
 		void IAstTransform.Run(AstNode node)

ICSharpCode.Decompiler/ILAst/ILCodes.cs
@@ -286,9 +286,12 @@ namespace ICSharpCode.Decompiler.ILAst
 		CallSetter,
 		/// <summary>Calls the setter of a instance property (or indexer)</summary>
 		CallvirtSetter,
-		/// <summary>Simulates getting the address of a property. Used as prefix on CallGetter or CallvirtGetter.</summary>
-		/// <remarks>Used for postincrement for properties, and to represent the Address() method on multi-dimensional arrays</remarks>
-		PropertyAddress
+		/// <summary>Simulates getting the address of the argument instruction.</summary>
+		/// <remarks>
+		/// Used for postincrement for properties, and to represent the Address() method on multi-dimensional arrays.
+		/// Also used when inlining a method call on a value type: "stloc(v, ...); call(M, ldloca(v));" becomes "call(M, AddressOf(...))"
+		/// </remarks>
+		AddressOf
 	}
 	
 	public static class ILCodeUtil

ICSharpCode.Decompiler/ILAst/InitializerPeepholeTransforms.cs
@@ -43,11 +43,14 @@ namespace ICSharpCode.Decompiler.ILAst
 					if (DecodeArrayInitializer(TypeAnalysis.GetTypeCode(arrayType), field.InitialValue, newArr)) {
 						body[pos] = new ILExpression(ILCode.Stloc, v, new ILExpression(ILCode.InitArray, arrayType, newArr));
 						body.RemoveAt(pos + 1);
+						new ILInlining(method).InlineIfPossible(body, ref pos);
 						return true;
 					}
 				}
 				
-				const int maxConsecutiveDefaultValueExpressions = 10;
+				// Put in a limit so that we don't consume too much memory if the code allocates a huge array
+				// and populates it extremely sparsly. However, 255 "null" elements in a row actually occur in the Mono C# compiler!
+				const int maxConsecutiveDefaultValueExpressions = 300;
 				List<ILExpression> operands = new List<ILExpression>();
 				int numberOfInstructionsToRemove = 0;
 				for (int j = pos + 1; j < body.Count; j++) {
@@ -72,6 +75,8 @@ namespace ICSharpCode.Decompiler.ILAst
 				if (operands.Count == arrayLength) {
 					expr.Arguments[0] = new ILExpression(ILCode.InitArray, arrayType, operands);
 					body.RemoveRange(pos + 1, numberOfInstructionsToRemove);
+					
+					new ILInlining(method).InlineIfPossible(body, ref pos);
 					return true;
 				}
 			}

ICSharpCode.Decompiler/ILAst/PeepholeTransform.cs
@@ -473,7 +473,7 @@ namespace ICSharpCode.Decompiler.ILAst
 			if (exprInit.Code == ILCode.Ldloc)
 				exprInit.Code = ILCode.Ldloca;
 			else if (exprInit.Code == ILCode.CallGetter)
-				exprInit.AddPrefix(new ILExpressionPrefix(ILCode.PropertyAddress));
+				exprInit = new ILExpression(ILCode.AddressOf, null, exprInit);
 			else
 				exprInit.Code = ILCode.Ldsflda;
 			expr.Arguments[0] = new ILExpression(incrementCode, incrementAmount, exprInit);
@@ -567,8 +567,8 @@ namespace ICSharpCode.Decompiler.ILAst
 			if (expr.Code == ILCode.Stobj) {
 				stloc.Arguments[0] = new ILExpression(ILCode.PostIncrement, incrementAmount, initialValue.Arguments[0]);
 			} else if (expr.Code == ILCode.CallSetter || expr.Code == ILCode.CallvirtSetter) {
+				initialValue = new ILExpression(ILCode.AddressOf, null, initialValue);
 				stloc.Arguments[0] = new ILExpression(ILCode.PostIncrement, incrementAmount, initialValue);
-				initialValue.AddPrefix(new ILExpressionPrefix(ILCode.PropertyAddress));
 			} else {
 				stloc.Arguments[0] = new ILExpression(ILCode.PostIncrement, incrementAmount, initialValue);
 				initialValue.Code = (expr.Code == ILCode.Stfld ? ILCode.Ldflda : ILCode.Ldelema);

ICSharpCode.Decompiler/ILAst/TypeAnalysis.cs
@@ -319,10 +319,7 @@ namespace ICSharpCode.Decompiler.ILAst
 						if (expr.Code == ILCode.CallSetter || expr.Code == ILCode.CallvirtSetter) {
 							return SubstituteTypeArgs(method.Parameters.Last().ParameterType, method);
 						} else {
-							TypeReference type = SubstituteTypeArgs(method.ReturnType, method);
-							if (expr.GetPrefix(ILCode.PropertyAddress) != null && !(type is ByReferenceType))
-								type = new ByReferenceType(type);
-							return type;
+							return SubstituteTypeArgs(method.ReturnType, method);
 						}
 					}
 				case ILCode.Newobj:
@@ -463,6 +460,11 @@ namespace ICSharpCode.Decompiler.ILAst
 						InferTypeForExpression(expr.Arguments[0], typeSystem.TypedReference);
 					}
 					return new ByReferenceType((TypeReference)expr.Operand);
+				case ILCode.AddressOf:
+					{
+						TypeReference t = InferTypeForExpression(expr.Arguments[0], UnpackPointer(expectedType));
+						return t != null ? new ByReferenceType(t) : null;
+					}
 					#endregion
 					#region Arithmetic instructions
 				case ILCode.Not: // bitwise complement

commit 310ae3f15fc5031d8121cbdda872b88526d537ef

Fixed issues introduced by #139 (Improved local variable inlining)

ICSharpCode.Decompiler/Ast/AstMethodBodyBuilder.cs
@@ -886,8 +886,6 @@ namespace ICSharpCode.Decompiler.Ast
 			
 			if (cecilMethod.Name == "Get" && cecilMethod.DeclaringType is ArrayType && methodArgs.Count > 1) {
 				return target.Indexer(methodArgs);
-			} else if (cecilMethod.Name == "Address" && cecilMethod.DeclaringType is ArrayType && methodArgs.Count > 1) {
-				return MakeRef(target.Indexer(methodArgs));
 			} else if (cecilMethod.Name == "Set" && cecilMethod.DeclaringType is ArrayType && methodArgs.Count > 2) {
 				return new AssignmentExpression(target.Indexer(methodArgs.GetRange(0, methodArgs.Count - 1)), methodArgs.Last());
 			}

ICSharpCode.Decompiler/ILAst/ILAstOptimizer.cs
@@ -324,6 +324,14 @@ namespace ICSharpCode.Decompiler.ILAst
 							expr.Code = ILCode.CallSetter;
 							break;
 						case "Address":
+							ByReferenceType brt = cecilMethod.ReturnType as ByReferenceType;
+							if (brt != null) {
+								MethodReference getMethod = new MethodReference("Get", brt.ElementType, cecilMethod.DeclaringType);
+								foreach (var p in cecilMethod.Parameters)
+									getMethod.Parameters.Add(p);
+								getMethod.HasThis = cecilMethod.HasThis;
+								expr.Operand = getMethod;
+							}
 							expr.Code = ILCode.CallGetter;
 							if (parentExpr != null) {
 								parentExpr.Arguments[posInParent] = new ILExpression(ILCode.AddressOf, null, expr);

ICSharpCode.Decompiler/ILAst/ILInlining.cs
@@ -184,7 +184,7 @@ namespace ICSharpCode.Decompiler.ILAst
 			int pos;
 			if (FindLoadInNext(next as ILExpression, v, inlinedExpression, out parent, out pos) == true) {
 				if (ldloc == 0) {
-					if (!IsGeneratedValueTypeTemporary((ILExpression)next, parent, pos, v))
+					if (!IsGeneratedValueTypeTemporary((ILExpression)next, parent, pos, v, inlinedExpression))
 						return false;
 				} else {
 					if (!aggressive && !v.IsGenerated && !NonAggressiveInlineInto((ILExpression)next, parent, inlinedExpression))
@@ -213,9 +213,69 @@ namespace ICSharpCode.Decompiler.ILAst
 		/// <param name="parent">The direct parent of the load within 'next'</param>
 		/// <param name="pos">Index of the load within 'parent'</param>
 		/// <param name="v">The variable being inlined.</param>
-		bool IsGeneratedValueTypeTemporary(ILExpression next, ILExpression parent, int pos, ILVariable v)
+		/// <param name="inlinedExpression">The expression being inlined</param>
+		bool IsGeneratedValueTypeTemporary(ILExpression next, ILExpression parent, int pos, ILVariable v, ILExpression inlinedExpression)
 		{
 			if (pos == 0 && v.Type != null && v.Type.IsValueType) {
+				// Inlining a value type variable is allowed only if the resulting code will maintain the semantics
+				// that the method is operating on a copy.
+				// Thus, we have to disallow inlining of other locals, fields, array elements, dereferenced pointers
+				switch (inlinedExpression.Code) {
+					case ILCode.Ldloc:
+					case ILCode.Stloc:
+					case ILCode.CompoundAssignment:
+					case ILCode.Ldelem_Any:
+					case ILCode.Ldelem_I:
+					case ILCode.Ldelem_I1:
+					case ILCode.Ldelem_I2:
+					case ILCode.Ldelem_I4:
+					case ILCode.Ldelem_I8:
+					case ILCode.Ldelem_R4:
+					case ILCode.Ldelem_R8:
+					case ILCode.Ldelem_Ref:
+					case ILCode.Ldelem_U1:
+					case ILCode.Ldelem_U2:
+					case ILCode.Ldelem_U4:
+					case ILCode.Ldobj:
+					case ILCode.Ldind_Ref:
+						return false;
+					case ILCode.Ldfld:
+					case ILCode.Stfld:
+					case ILCode.Ldsfld:
+					case ILCode.Stsfld:
+						// allow inlining field access only if it's a readonly field
+						FieldDefinition f = ((FieldReference)inlinedExpression.Operand).Resolve();
+						if (!(f != null && f.IsInitOnly))
+							return false;
+						break;
+					case ILCode.Call:
+					case ILCode.CallGetter:
+						// inlining runs both before and after IntroducePropertyAccessInstructions,
+						// so we have to handle both 'call' and 'callgetter'
+						MethodReference mr = (MethodReference)inlinedExpression.Operand;
+						// ensure that it's not an multi-dimensional array getter
+						if (mr.DeclaringType is ArrayType)
+							return false;
+						goto case ILCode.Callvirt;
+					case ILCode.Callvirt:
+					case ILCode.CallvirtGetter:
+						// don't inline foreach loop variables:
+						mr = (MethodReference)inlinedExpression.Operand;
+						if (mr.Name == "get_Current" && mr.HasThis)
+							return false;
+						break;
+					case ILCode.Castclass:
+					case ILCode.Unbox_Any:
+						// These are valid, but might occur as part of a foreach loop variable.
+						ILExpression arg = inlinedExpression.Arguments[0];
+						if (arg.Code == ILCode.CallGetter || arg.Code == ILCode.CallvirtGetter || arg.Code == ILCode.Call || arg.Code == ILCode.Callvirt) {
+							mr = (MethodReference)arg.Operand;
+							if (mr.Name == "get_Current" && mr.HasThis)
+								return false; // looks like a foreach loop variable, so don't inline it
+						}
+						break;
+				}
+				
 				// inline the compiler-generated variable that are used when accessing a member on a value type:
 				switch (parent.Code) {
 					case ILCode.Call:
@@ -240,8 +300,12 @@ namespace ICSharpCode.Decompiler.ILAst
 		/// </summary>
 		/// <param name="next">The next top-level expression</param>
 		/// <param name="parent">The direct parent of the load within 'next'</param>
-		bool NonAggressiveInlineInto(ILExpression next, ILExpression parent)
+		/// <param name="inlinedExpression">The expression being inlined</param>
+		bool NonAggressiveInlineInto(ILExpression next, ILExpression parent, ILExpression inlinedExpression)
 		{
+			if (inlinedExpression.Code == ILCode.DefaultValue)
+				return true;
+			
 			switch (next.Code) {
 				case ILCode.Ret:
 				case ILCode.Brtrue:
@@ -253,28 +317,6 @@ namespace ICSharpCode.Decompiler.ILAst
 			}
 		}
 		
-		bool NonAggressiveInlineInto(ILExpression next, ILExpression parent, ILExpression inlinedExpression)
-		{
-			switch (inlinedExpression.Code) {
-				case ILCode.InitArray:
-				case ILCode.InitObject:
-				case ILCode.InitCollection:
-				case ILCode.DefaultValue:
-					return true;
-			}
-			
-			switch (next.Code) {
-				case ILCode.Ret:
-					return parent.Code == ILCode.Ret;
-				case ILCode.Brtrue:
-					return parent.Code == ILCode.Brtrue;
-				case ILCode.Switch:
-					return parent.Code == ILCode.Switch || parent.Code == ILCode.Sub;
-				default:
-					return false;
-			}
-		}
-
 		/// <summary>
 		/// Gets whether 'expressionBeingMoved' can be inlined into 'expr'.
 		/// </summary>

ICSharpCode.Decompiler/Tests/ValueTypes.cs
@@ -35,6 +35,17 @@ public static class ValueTypes
 		}
 	}
 	
+	private static readonly ValueTypes.S ReadOnlyS = default(ValueTypes.S);
+	private static ValueTypes.S MutableS = default(ValueTypes.S);
+	
+	public static void CallMethodViaField()
+	{
+		ValueTypes.ReadOnlyS.SetField();
+		ValueTypes.MutableS.SetField();
+		ValueTypes.S mutableS = ValueTypes.MutableS;
+		mutableS.SetField();
+	}
+	
 	public static ValueTypes.S InitObj1()
 	{
 		ValueTypes.S result = default(ValueTypes.S);
@@ -125,5 +136,12 @@ public static class ValueTypes
 	public static ValueTypes.S? As(object obj)
 	{
 		return obj as ValueTypes.S?;
+	}
+	
+	public static ValueTypes.S OnlyChangeTheCopy(ValueTypes.S p)
+	{
+		ValueTypes.S s = p;
+		s.SetField();
+		return p;
 	}
 }