icsharpcode/ILSpy

commit 7553c120a565ae798f0282663c34728aaa95ac53

Fix #1497: do not insert newlines inside string interpolation expressions.

ICSharpCode.Decompiler/CSharp/OutputVisitor/CSharpOutputVisitor.cs
@@ -292,16 +292,19 @@ namespace ICSharpCode.Decompiler.CSharp.OutputVisitor
 			return callChainLength;
 		}
 
-		protected virtual void InsertNewLineWhenInMethodCallChain(MemberReferenceExpression expr)
+		protected virtual bool InsertNewLineWhenInMethodCallChain(MemberReferenceExpression expr)
 		{
 			int callChainLength = GetCallChainLengthLimited(expr);
-			if (callChainLength < 3) return;
+			if (callChainLength < 3) return false;
+			if (expr.GetParent(n => n is Statement || n is LambdaExpression || n is InterpolatedStringContent) is InterpolatedStringContent)
+				return false;
 			if (callChainLength == 3)
 				writer.Indent();
 			writer.NewLine();
 			
 			isAtStartOfLine = true;
 			isAfterSpace = false;
+			return true;
 		}
 		
 		protected virtual void OpenBrace(BraceStyle style)
@@ -899,13 +902,12 @@ namespace ICSharpCode.Decompiler.CSharp.OutputVisitor
 		{
 			StartNode(memberReferenceExpression);
 			memberReferenceExpression.Target.AcceptVisitor(this);
-			InsertNewLineWhenInMethodCallChain(memberReferenceExpression);
+			bool insertedNewLine = InsertNewLineWhenInMethodCallChain(memberReferenceExpression);
 			WriteToken(Roles.Dot);
 			WriteIdentifier(memberReferenceExpression.MemberNameToken);
 			WriteTypeArguments(memberReferenceExpression.TypeArguments);
-			if (!(memberReferenceExpression.Parent is InvocationExpression)) {
-				if (GetCallChainLengthLimited(memberReferenceExpression) >= 3)
-					writer.Unindent();
+			if (insertedNewLine) {
+				writer.Unindent();
 			}
 			EndNode(memberReferenceExpression);
 		}

ICSharpCode.Decompiler.Tests/TestCases/Pretty/CS6_StringInterpolation.cs
@@ -97,6 +97,11 @@ namespace ICSharpCode.Decompiler.Tests.TestCases.Pretty
 			RequiresCast((IFormattable)$"\ta{$"a{args.Length}" == args[0]}");
 		}
 
+		public void Issue1497(string[] args)
+		{
+			Console.WriteLine($"args[0]: {args[0].Trim(':').Trim('&').Trim(':').Trim('&')} asdf {args.Length:x} test");
+		}
+
 		public void RequiresCast(string value)
 		{
 		}

winnerdev2012/unity2023-ChatGPT

commit 4aa8674752b7d3d3c7059086a94a5148c098f45a

Fix regression introduced by 7553c120a565ae798f0282663c34728aaa95ac53

ICSharpCode.Decompiler/CSharp/OutputVisitor/CSharpOutputVisitor.cs
@@ -906,7 +906,7 @@ namespace ICSharpCode.Decompiler.CSharp.OutputVisitor
 			WriteToken(Roles.Dot);
 			WriteIdentifier(memberReferenceExpression.MemberNameToken);
 			WriteTypeArguments(memberReferenceExpression.TypeArguments);
-			if (insertedNewLine) {
+			if (insertedNewLine && !(memberReferenceExpression.Parent is InvocationExpression)) {
 				writer.Unindent();
 			}
 			EndNode(memberReferenceExpression);